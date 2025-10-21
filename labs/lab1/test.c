f89a311a14a28c816fdc90d6e20889741047714a
trevor <trevorwt95@gmail.com>
2025-10-21 15:41:12 -0700

    cae working

--- labs/lab1/test.c
+++ labs/lab1/test.c
@@ -11,13 +11,12 @@ enum { ASCII_MIN = 32, ASCII_MAX = 126, ASCII_RANGE = 95 };
 
 int main(int argc, char *argv[]) {
   int opt;
-  const char *cea_key = "";
-  int cea_key_len = 0;
+  const char *cae_key = "";
+  int cae_key_len = 0;
   int mode = 1; // 1 - encrypt, -1 - decrypt
   char buf[4096];
   ssize_t n;
 
-  printf("cea_key = %s\n", cea_key);
   while ((opt = getopt(argc, argv, "edc:x:h")) != -1) {
     switch (opt) {
     case 'e':
@@ -27,8 +26,7 @@ int main(int argc, char *argv[]) {
       mode = -1;
       break;
     case 'c':
-      cea_key = optarg;
-      printf("cea_key = %s\n", cea_key);
+      cae_key = optarg;
       break;
     case 'x':
       break;
@@ -39,25 +37,31 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  while (cea_key[cea_key_len] != '\0')
-    ++cea_key_len;
+  while (cae_key[cae_key_len] != '\0')
+    ++cae_key_len;
 
   while ((n = read(STDIN_FILENO, buf, sizeof buf)) > 0) {
-    unsigned char kch;
-    int shift, off;
+    unsigned char key_char;
+    int shift, offset;
+    int key_pos = 0;
+
     for (ssize_t i = 0; i < n; ++i) {
       unsigned char c = (unsigned char)buf[i];
 
-      if (c < ASCII_MIN || c > ASCII_MAX || cea_key_len == 0)
+      if (c < ASCII_MIN || c > ASCII_MAX || cae_key_len == 0){
+        key_pos = 0;
         continue;
+    }
 
-      kch = (unsigned char)cea_key[i % cea_key_len];
-      shift = (int)kch - ASCII_MIN;
-      off = (int)c - ASCII_MIN;
-      off = (off + mode * shift + ASCII_RANGE) % ASCII_RANGE;
-      buf[i] = (char)(ASCII_MIN + off);
+      key_char = (unsigned char)cae_key[key_pos % cae_key_len];
+      key_pos++;
+      shift = (int)key_char - ASCII_MIN;
+      offset = (int)c - ASCII_MIN;
+      offset = (offset + mode * shift + ASCII_RANGE) % ASCII_RANGE;
+      buf[i] = (char)(ASCII_MIN + offset);
     }
     write(STDOUT_FILENO, buf, (size_t)n);
+
   }
   return 0;
 }
