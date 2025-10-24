f8b20b50e96b6aac80f1eb13bf804f30c2077439
Trevor Thompson <tthompso@pdx.edu>
2025-10-24 12:57:57 -0700

    finishing mystat

--- labs/lab1/mystat.c
+++ labs/lab1/mystat.c
@@ -1,25 +1,32 @@
-#include <grp.h>
 #include <limits.h>
-#include <pwd.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/sysmacros.h>
 #include <time.h>
 #include <unistd.h>
+#include <grp.h>
+#include <pwd.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
 
 int main(int argc, char **argv) {
+
+  // info
   struct stat sb;
   struct passwd *pw;
   struct group *gr;
-  char str[11];
+
+  //mode
+  char mode_str[11];
   char permission_chars[] = "rwxrwxrwx";
   mode_t permission_bits[] = {
     S_IRUSR, S_IWUSR, S_IXUSR, // User: read, write, execute
     S_IRGRP, S_IWGRP, S_IXGRP, // Group: read, write, execute
     S_IROTH, S_IWOTH, S_IXOTH  // Other: read, write, execute
   };
+
+  // time
+  char buffer[100];
 
   if (argc <= 1) {
     printf("Usage: ./mystat <filename1> ... <filenameN>\n");
@@ -31,62 +38,62 @@ int main(int argc, char **argv) {
       fprintf(stderr, "*** Failed to stat file '%s', skipping.\n",argv[i]);
       fprintf(stderr,"***  File '%s', does not exist or we done have access.\n",argv[i]);
       fprintf(stderr,"*** Could not stat file: No such file or directory.\n");
+
     } else {
       printf("File: %s\n", argv[i]);
       printf("  %-26s", "File type:");
+
       switch (sb.st_mode & S_IFMT) {
       case S_IFBLK:
-        str[0] = 'b';
+        mode_str[0] = 'b';
         printf("block device\n");
         break;
       case S_IFCHR:
-        str[0] = 'c';
+        mode_str[0] = 'c';
         printf("character device\n");
         break;
       case S_IFDIR:
-        str[0] = 'd';
+        mode_str[0] = 'd';
         printf("directory\n");
         break;
       case S_IFIFO:
-        str[0] = 'p';
+        mode_str[0] = 'p';
         printf("FIFO/pipe\n");
         break;
       case S_IFREG:
-        str[0] = '-';
+        mode_str[0] = '-';
         printf("regular file\n");
         break;
       case S_IFSOCK:
-        str[0] = 's';
+        mode_str[0] = 's';
         printf("socket\n");
         break;
       case S_IFLNK:
         char link_target[PATH_MAX];
         char resolved_target[PATH_MAX];
         ssize_t len = readlink(argv[i], link_target, sizeof(link_target) - 1);
-        str[0] = 'l';
+        mode_str[0] = 'l';
         if (len != -1) link_target[len] = '\0';
         if (realpath(link_target, resolved_target) != NULL) printf("Symbolic link -> %s\n", resolved_target);
         else printf("Symbolic link - with dangling destination\n");
         break;
-      default:
-        str[0] = '?';
-        printf("unknown?\n");
-        break;
       }
 
-      for (int j = 0; j < 9; j++) str[j + 1] = (sb.st_mode & permission_bits[j]) ? permission_chars[j] : '-';
-      str[10] = '\0';
+      // mode
+      for (int j = 0; j < 9; j++) mode_str[j + 1] = (sb.st_mode & permission_bits[j]) ? permission_chars[j] : '-';
+      mode_str[10] = '\0';
 
-      if (sb.st_mode & S_ISUID) str[3] = (str[3]=='x') ? 's' : 'S';
-      if (sb.st_mode & S_ISGID) str[6] = (str[6]=='x') ? 's' : 'S';
-      if (sb.st_mode & S_ISVTX) str[9] = (str[9]=='x') ? 't' : 'T';
+      if (sb.st_mode & S_ISUID) mode_str[3] = (mode_str[3]=='x') ? 's' : 'S';
+      if (sb.st_mode & S_ISGID) mode_str[6] = (mode_str[6]=='x') ? 's' : 'S';
+      if (sb.st_mode & S_ISVTX) mode_str[9] = (mode_str[9]=='x') ? 't' : 'T';
 
+      // info
       pw = getpwuid(sb.st_uid);
       gr = getgrgid(sb.st_gid);
 
       printf("  %-26s%lu\n", "Device ID number:", (unsigned long)sb.st_dev);
       printf("  %-26s%ju\n", "I-node number:", (uintmax_t)sb.st_ino);
-      printf("  %-26s%-18s(%jo in octal)\n", "Mode:", str, (uintmax_t)(sb.st_mode & 0777));
+      printf("  %-26s%-18s(%jo in octal)\n", "Mode:", mode_str, (uintmax_t)(sb.st_mode & 0777));
       printf("  %-26s%ju\n", "Link count:", (uintmax_t)sb.st_nlink);
       printf("  %-26s%-18s(UID = %ju)\n", "Owner Id:", pw->pw_name, (uintmax_t)sb.st_uid);
       printf("  %-26s%-18s(GID = %ju)\n", "Group Id:", gr->gr_name, (uintmax_t)sb.st_gid);
@@ -94,9 +101,30 @@ int main(int argc, char **argv) {
       printf("  %-26s%jd bytes\n", "File size:", (intmax_t)sb.st_size);
       printf("  %-26s%jd\n", "Blocks allocated:", (intmax_t)sb.st_blocks);
 
-      printf("  %-26s%s", "Last file access:", ctime(&sb.st_atime));
-      printf("  %-26s%s", "Last file modification:", ctime(&sb.st_mtime));
-      printf("  %-26s%s", "Last status change:", ctime(&sb.st_ctime));
+      // Time
+      printf("  %-26s%ld (seconds since the epoch)\n","Last file access:",(long)sb.st_atime);
+      printf("  %-26s%ld (seconds since the epoch)\n","Last file modification:",(long)sb.st_mtime);
+      printf("  %-26s%ld (seconds since the epoch)\n","Last file change:",(long)sb.st_ctime);
+
+
+      strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S %z (%Z) %a (local)", localtime(&sb.st_atime));
+        printf("  %-26s%s\n", "Last file access:", buffer);
+
+      strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S %z (%Z) %a (local)", localtime(&sb.st_mtime));
+        printf("  %-26s%s\n", "Last file modification:", buffer);
+
+      strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S %z (%Z) %a (local)", localtime(&sb.st_ctime));
+        printf("  %-26s%s\n", "Last file change:", buffer);
+
+      strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S %z (%Z) %a (GMT)", gmtime(&sb.st_atime));
+        printf("  %-26s%s\n", "Last file access:", buffer);
+
+      strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S %z (%Z) %a (GMT)", gmtime(&sb.st_mtime));
+        printf("  %-26s%s\n", "Last file modification:", buffer);
+
+      strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S %z (%Z) %a (GMT)", gmtime(&sb.st_ctime));
+        printf("  %-26s%s\n", "Last file change:", buffer);
+
     }
   }
   return 0;
